a186a4ded7bc5fd48f6418558ca41ca452ec4c56
Xavier Rolex <xavierrolex7@gmail.com>
2025-05-16 03:12:30 +0600

    Added CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.schemas import NewsInput, PredictionResult
 from app.predictor import predict
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Need to update with frontend URL in production like allow_origins=["https://your-frontend-domain.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(
     title="Fake News Detector API",
